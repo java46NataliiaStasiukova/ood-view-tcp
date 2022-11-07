74ac23cf20949c5690eabee60d2d56b6142850ef
Nataliia <nstasiukova@gmail.com>
2022-11-07 13:10:19 +0200

    Homework-37

--- src/telran/view/DateOperationsMenu.java
+++ src/telran/view/DateOperationsMenu.java
@@ -22,40 +22,25 @@ public class DateOperationsMenu {
 		return res;
 	}
 	static void getDateAfter(InputOutput io) {
-		int days = enterDaysNumber(io);
+		int days = io.readInt("enter number of days", "no number", 0, Integer.MAX_VALUE);
 		LocalDate date = LocalDate.now();
 		io.writeLine(date.plusDays(days));
 		
 	}
 	static void getDateBefore(InputOutput io) {
-		int days = enterDaysNumber(io);
+		int days = io.readInt("enter number of days", "no number", 0, Integer.MAX_VALUE);
 		LocalDate date = LocalDate.now();
 		io.writeLine(date.minusDays(days));
 		
 	}
 	static void getDaysBetween(InputOutput io) {
-		LocalDate[] dates = enterTwoDates(io);
-		io.writeLine(ChronoUnit.DAYS.between(dates[0], dates[1]));
+		io.writeLine(ChronoUnit.DAYS.between(io.readDate("enter first date", "no date"), 
+				io.readDate("enter second date", "ni date")));
 	}
 
 	static void getAge(InputOutput io) {
-		LocalDate date = enterDate(io);
+		LocalDate date = io.readDate("enter birthday date", "no date");
 		io.writeLine(ChronoUnit.YEARS.between(date, LocalDate.now()));
 	}
-	private static int enterDaysNumber(InputOutput io) {
-		int res = io.readInt("enter number of days", "no number", 0, Integer.MAX_VALUE);
-		return res;
-	}
-	private static LocalDate[] enterTwoDates(InputOutput io) {
-		LocalDate[] res = new LocalDate[2];
-		res[0] = io.readDate("enter first date", "no date");
-		res[1] = io.readDate("enter second date", "ni date");
-		return res;
-	}
-	private static LocalDate enterDate(InputOutput io) {
-		LocalDate res = io.readDate("enter birthday date", "no date");
-		return res;
-	}
-	
 
 }
