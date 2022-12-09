2a8920eefa762fa5aa23788f07ce2f77adab5bb9
Nataliia <nstasiukova@gmail.com>
2022-12-09 16:43:16 +0200

    Homework-46

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -1,15 +1,21 @@
 package telran.net;
 import java.net.*;
-import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 public class TcpServer implements Runnable {
-	private ServerSocket serverSocket;
-	private int port;
-	private ApplProtocol protocol;
-	public TcpServer(int port, ApplProtocol protocol) throws Exception {
-		this.port = port;
-		this.protocol = protocol;
-		serverSocket = new ServerSocket(port);
-	}
+	
+	private static final int N_THREADS = 2;
+	public ExecutorService executor;
+	
+   private ServerSocket serverSocket;
+   private int port;
+   private ApplProtocol protocol;
+   public TcpServer(int port, ApplProtocol protocol) throws Exception{
+	   this.port = port;
+	   this.protocol = protocol;
+	   executor = Executors.newFixedThreadPool(N_THREADS);
+	   serverSocket = new ServerSocket(port);
+   }
 	@Override
 	public void run() {
 		System.out.println("Server is listening on the port " + port);
@@ -17,13 +23,12 @@ public class TcpServer implements Runnable {
 			while(true) {
 				Socket socket = serverSocket.accept();
 				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				//clientServer.run();
-				Thread thread = new Thread(clientServer);
-				thread.run();
+				executor.execute(clientServer);
 			}
 		} catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
+		
 
 	}
 
